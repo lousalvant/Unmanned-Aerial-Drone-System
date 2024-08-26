466731e9a2fc690512ae0e74ab6d4657ebea39d4
lousalvant <lousalvant@gmail.com>
2024-08-26 19:19:05 -0400

    map coordinates fixed

--- gcs-front-end/src/components/MapComponent.js
+++ gcs-front-end/src/components/MapComponent.js
@@ -6,6 +6,7 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import markerRetina from 'leaflet/dist/images/marker-icon-2x.png'; // Retina marker
 
 const defaultPosition = [51.505, -0.09]; // Default starting position
 
@@ -18,9 +19,9 @@ const MapComponent = () => {
         const res = await fetch("http://localhost:8081/gps");
         if (res.ok) {
           const data = await res.json();
-          // Only update if valid latitude and longitude are present
           if (typeof data.latitude_deg === 'number' && typeof data.longitude_deg === 'number' &&
               data.latitude_deg !== 0 && data.longitude_deg !== 0) {
+            console.log("Valid GPS Data:", data); // Debugging line
             setGpsPos([data.latitude_deg, data.longitude_deg]);
           } else {
             console.warn("Received invalid GPS data:", data);
@@ -31,7 +32,7 @@ const MapComponent = () => {
       } catch (error) {
         console.error("Error fetching GPS data:", error);
       }
-    }, 500); // Fetch every 500 ms
+    }, 500); // Fetch every 500ms
 
     return () => clearInterval(timer);
   }, []);
@@ -39,13 +40,18 @@ const MapComponent = () => {
   // Fix marker icon issues with Leaflet in React
   const DefaultIcon = L.icon({
     iconUrl: markerIcon,
+    iconRetinaUrl: markerRetina,
     shadowUrl: markerShadow,
+    iconSize: [25, 41], // Size of the icon
+    iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+    popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
+    shadowSize: [41, 41] // Size of the shadow
   });
 
   L.Marker.prototype.options.icon = DefaultIcon;
 
   return (
-    <MapContainer center={gpsPos} zoom={13} style={{ height: '400px', width: '60%' }}>
+    <MapContainer key={gpsPos.toString()} center={gpsPos} zoom={13} style={{ height: '400px', width: '60%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
