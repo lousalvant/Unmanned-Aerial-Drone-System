25cb65b0209f385b2074c19545d7a11873730d1e
lousalvant <lousalvant@gmail.com>
2024-08-26 19:26:12 -0400

    map refreshing fix

--- gcs-front-end/src/components/MapComponent.js
+++ gcs-front-end/src/components/MapComponent.js
@@ -1,7 +1,7 @@
 // MapComponent.js
 
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
@@ -9,6 +9,23 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import markerRetina from 'leaflet/dist/images/marker-icon-2x.png'; // Retina marker
 
 const defaultPosition = [51.505, -0.09]; // Default starting position
+
+const DroneMarker = ({ position }) => {
+  const map = useMap(); // Get the map instance
+  useEffect(() => {
+    if (position) {
+      map.setView(position); // Update the map center
+    }
+  }, [position, map]);
+
+  return (
+    <Marker position={position}>
+      <Popup>
+        Drone is at latitude: {position[0]}, longitude: {position[1]}
+      </Popup>
+    </Marker>
+  );
+};
 
 const MapComponent = () => {
   const [gpsPos, setGpsPos] = useState(defaultPosition);
@@ -21,7 +38,6 @@ const MapComponent = () => {
           const data = await res.json();
           if (typeof data.latitude_deg === 'number' && typeof data.longitude_deg === 'number' &&
               data.latitude_deg !== 0 && data.longitude_deg !== 0) {
-            console.log("Valid GPS Data:", data); // Debugging line
             setGpsPos([data.latitude_deg, data.longitude_deg]);
           } else {
             console.warn("Received invalid GPS data:", data);
@@ -51,18 +67,12 @@ const MapComponent = () => {
   L.Marker.prototype.options.icon = DefaultIcon;
 
   return (
-    <MapContainer key={gpsPos.toString()} center={gpsPos} zoom={13} style={{ height: '400px', width: '60%' }}>
+    <MapContainer center={defaultPosition} zoom={13} style={{ height: '400px', width: '60%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {gpsPos && (
-        <Marker position={gpsPos}>
-          <Popup>
-            Drone is at latitude: {gpsPos[0]}, longitude: {gpsPos[1]}
-          </Popup>
-        </Marker>
-      )}
+      <DroneMarker position={gpsPos} />
     </MapContainer>
   );
 };
